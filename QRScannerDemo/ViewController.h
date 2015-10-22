bed26c6551eac9d0974495878ce636ce11f81f27
fengarenas <fengarenas@126.com>
2015-10-22 11:11:12 +0800

    Initial Commit

---
+++ QRScannerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRScannerDemo
+//
+//  Created by Fengj on 15/10/22.
+//  Copyright © 2015年 FengJun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QRScannerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QRScannerDemo
+//
+//  Created by Fengj on 15/10/22.
+//  Copyright © 2015年 FengJun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
