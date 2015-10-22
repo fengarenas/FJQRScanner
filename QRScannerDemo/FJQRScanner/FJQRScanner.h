bc7cc45b6412b6e703df35a784090e11fdebf683
fengarenas <fengarenas@126.com>
2015-10-22 14:20:43 +0800

    copyright2

--- QRScannerDemo/FJQRScanner/FJQRScanner.h
+++ QRScannerDemo/FJQRScanner/FJQRScanner.h
@@ -1,5 +1,7 @@
-//
-// Copyright (c) 2013 QRScanner (http://devfeng.com/)
+// Copyright (c) 2013 FJBannerView
+// Author fengjun
+// Blog:http://devfeng.com/
+// Url :https://github.com/fengarenas/FJQRScanner
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -18,7 +20,6 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
--- QRScannerDemo/FJQRScanner/FJQRScanner.m
+++ QRScannerDemo/FJQRScanner/FJQRScanner.m
@@ -1,5 +1,7 @@
-//
-// Copyright (c) 2013 QRScanner (http://devfeng.com/)
+// Copyright (c) 2013 FJBannerView
+// Author fengjun
+// Blog:http://devfeng.com/
+// Url :https://github.com/fengarenas/FJQRScanner
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
